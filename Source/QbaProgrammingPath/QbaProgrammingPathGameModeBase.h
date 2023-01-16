32086af205ce728d1a75efdb82e3a33b04312e36
qbaunreal <122815600+qbaunreal@users.noreply.github.com>
2023-01-16 18:01:41 +0100

    Init

---
+++ Source/QbaProgrammingPath/QbaProgrammingPathGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "QbaProgrammingPathGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QBAPROGRAMMINGPATH_API AQbaProgrammingPathGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
